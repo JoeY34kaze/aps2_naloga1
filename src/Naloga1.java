903a7f9b5431ebbe2a55915969c592cb644b7605
JoeY34kaze <ragnar1724@gmail.com>
2015-04-06 16:57:20 +0200

    narejen ss

--- src/Naloga1.java
+++ src/Naloga1.java
@@ -1,60 +1,42 @@
+import java.util.Scanner;
+
 public class Naloga1 {
 	public static String nacin; //tr || co
 	public static String algo; //ss,ms,hs,...
 	public static String order;//asc, desc
-	public static int table_length;
+	public static int table_length=-1;
 
 
-	public static void main(String[] args) {
-		set_arg(args, args.length);
-		
-		
-		
-		
-		run_algo(read_intArray_from_stdin(),nacin, algo, order, table_length);
-	}
 	
-	private static int[] read_intArray_from_stdin() {//za napisat
-		// TODO Auto-generated method stub
+	
+	/*private static int[] read_intArray_from_stdin() {//za napisat
+		int vel=1;
+		if(table_length!= -1){vel=table_length;}
+		else{table_length=0;}
+		int[] rez= new int[vel];
+		Scanner s=new Scanner(System.in);
+		int i=0;
+		while (s.hasNextInt()) {
+			//System.out.println("aaa");
+			if(i>=rez.length){//ce je vecji je treba tabelo alocirat naprej
+				if(vel<2){vel=2;}
+				else {vel*=2;}
+				rez=double_array(rez,vel);
+			}
+		        rez[i]=s.nextInt();
+		        i++;
+		        table_length++;//globaln stevec za stevilo elementov ce bo treba
+		        System.out.println("i: "+i);
+		     
+		  }
+		s.close();
+		
 		int[] r= {8, 5, 6, 1, 7, 2};
+		for(int n:rez){System.out.printf("%d",n);}
 		return r;
-	}
+	} */
 
-	private static void set_arg(String[] a, int b){
-		if(b>3){table_length=Integer.parseInt(a[3]);}
-		if(b>2){order=a[2];}
-		if(b>1){algo=a[1];}
-		if(b>0){nacin=a[0];}
-	}
-
-	public static void run_algo(int[] in, String nac, String al, String ord,
-			int len) {
-		if (al.equals("bs") && ord.equals("up")) {
-			int[] urejen = bs_asc(in, len);
-			if (nacin.equals("count")) {// se druga dva nacina za count
-				bs_asc(urejen, len);
-				bs_desc(urejen, len);
-			}
-		} else if (al.equals("bs") && ord.equals("down")) {
-			int[] urejen = bs_desc(in, len);
-			if (nacin.equals("count")) {// se druga dva nacina za count
-				bs_desc(urejen, len);
-				bs_asc(urejen, len);
-			}
-		} else {
-		}
-	}
-	
-	public static void izpisi_sled(int[] in, int dolzina_urejenega_dela){
-		String iz="";
-		for(int i=0;i<in.length;i++){
-			if(i==dolzina_urejenega_dela){iz=iz+"| ";}
-			iz=iz+in[i]+" ";
-		}
-		System.out.println(iz);
-	}
-
-	private static int[] bs_asc(int[] in, int len) {//vrne st primerjav in premikov v tabeli    bs asc naj bi bil zakljucen
+	private static int[] bs_asc(int[] in, int len) {//   bs asc naj bi bil zakljucen
 		int prim=0;
 		int prem=0;
 		int i;
@@ -83,13 +65,11 @@ public class Naloga1 {
 		}
 		if(nacin.equals("count")){System.out.println(prim+" "+prem);} //dela
 		return in;
-
-		
-		
-		
-	}
 	
-	private static int[] bs_desc(int[] in,int len) {//vrne st primerjav in premikov v tabeli
+}
+
+	private static int[] bs_desc(int[] in,int len) {//
+
 		int prim=0;
 		int prem=0;
 		int i;
@@ -119,6 +99,163 @@ public class Naloga1 {
 		if(nacin.equals("count")){System.out.println(prim+" "+prem);} //dela
 		return in;
 		
+	}
+
+	private static int[] ss_asc(int[] in, int len) {//algo dela zakon
+		int prim=0;
+		int prem=0;
+		int a=0;
+		int b=in.length-1;
+		int min=in[b];
+		int i_min=b;
+		while(a<b){
+			if(nacin.equals("trace")){izpisi_sled(in, a);}
+			for(int i=b;i>a;i--){
+				//get min
+				if(in[i-1]<min){
+					min=in[i-1];
+					i_min=i-1;
+				}
+				prim++;
+			}
+			//swap
+			int temp=in[a];
+			in[a]=in[i_min];
+			in[i_min]=temp;
+			min=in[b];
+			i_min=b;
+			a++;
+			prem+=3;
+		}
+		if(nacin.equals("trace")){izpisi_sled(in, a);}
+		if(nacin.equals("count")){System.out.println(prim+" "+prem);} //dela
+		return in;
+	}
+
+	private static int[] ss_desc(int[] in, int len) {
+		int prim=0;
+		int prem=0;
+		int a=0;
+		int b=in.length-1;
+		int max=in[b];
+		int i_max=b;
+		while(a<b){
+			if(nacin.equals("trace")){izpisi_sled(in, a);}
+			for(int i=b;i>a;i--){
+				//get min
+				if(in[i-1]>max){
+					max=in[i-1];
+					i_max=i-1;
+				}
+				prim++;
+			}
+			//swap
+			int temp=in[a];
+			in[a]=in[i_max];
+			in[i_max]=temp;
+			max=in[b];
+			i_max=b;
+			a++;
+			prem+=3;
+		}
+		if(nacin.equals("trace")){izpisi_sled(in, a);}
+		if(nacin.equals("count")){System.out.println(prim+" "+prem);} //dela
+		return in;
+		
+	}
+	public static void main(String[] args) {
+		set_arg(args, args.length);
+		
+		
+		
+		//int[] a=read_intArray_from_stdin();
+		int[] a= {8,5,6,1,7,2};
+		run_algo(a,nacin, algo, order, table_length);
+	}
+
+	public static void izpisi_sled(int[] in, int dolzina_urejenega_dela){
+		String iz="";
+		for(int i=0;i<in.length;i++){
+			if(i==dolzina_urejenega_dela){iz=iz+"| ";}
+			iz=iz+in[i]+" ";
+		}
+		System.out.println(iz);
+	}
+
+	public static void run_algo(int[] in, String nac, String al, String ord, int len) {
+		if (al.equals("bs") && ord.equals("up")) {
+			int[] urejen = bs_asc(in, len);
+			if (nacin.equals("count")) {// se druga dva nacina za count
+				bs_asc(urejen, len);
+				bs_desc(urejen, len);
+			}
+		} else if (al.equals("bs") && ord.equals("down")) {
+			int[] urejen = bs_desc(in, len);
+			if (nacin.equals("count")) {// se druga dva nacina za count
+				bs_desc(urejen, len);
+				bs_asc(urejen, len);
+			}
+		}else if (al.equals("ss") && ord.equals("up")) {
+			int[] urejen = ss_asc(in, len);
+			if (nacin.equals("count")) {// se druga dva nacina za count
+				ss_asc(urejen, len);
+				ss_desc(urejen, len);
+			}
+		}
+		else if (al.equals("ss") && ord.equals("down")) {
+			int[] urejen = ss_desc(in, len);
+			if (nacin.equals("count")) {// se druga dva nacina za count
+				ss_desc(urejen, len);
+				ss_asc(urejen, len);
+			}
+			
+	
+		}else{}
+	}
+
+	private static int[] double_array(int[] a, int vel){
+		if(vel<2){vel=2;}
+		else {vel*=2;}
+		int [] rez2= new int[vel];
+		//prepisat array i guess
+		   for(int j = 0; j < a.length; ++j) {
+			      rez2[j] = a[j];
+			    }
+		   return rez2;//priredit rez<-rez2
+	}
+
+	/*private static int[] read_intArray_from_stdin() {//za napisat
+		int vel=1;
+		if(table_length!= -1){vel=table_length;}
+		else{table_length=0;}
+		int[] rez= new int[vel];
+		Scanner s=new Scanner(System.in);
+		int i=0;
+		while (s.hasNextInt()) {
+			//System.out.println("aaa");
+			if(i>=rez.length){//ce je vecji je treba tabelo alocirat naprej
+				if(vel<2){vel=2;}
+				else {vel*=2;}
+				rez=double_array(rez,vel);
+			}
+		        rez[i]=s.nextInt();
+		        i++;
+		        table_length++;//globaln stevec za stevilo elementov ce bo treba
+		        System.out.println("i: "+i);
+		     
+		  }
+		s.close();
+		
+		int[] r= {8, 5, 6, 1, 7, 2};
+		for(int n:rez){System.out.printf("%d",n);}
+		return r;
+	} */
+	
+	private static void set_arg(String[] a, int b){
+		if(b>3){table_length=Integer.parseInt(a[3]);}
+		if(b>2){order=a[2];}
+		if(b>1){algo=a[1];}
+		if(b>0){nacin=a[0];}
 	}
 }
 
